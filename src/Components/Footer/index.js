efbeb1d5ff0203ff284d7a6762a0fc8beae0061e
Shanu Kumar <shanukumar467@gmail.com>
2023-04-20 12:10:01 +0530

    handleSubmit updated

--- src/Components/Footer/index.js
+++ src/Components/Footer/index.js
@@ -4,8 +4,8 @@ import "./style.css"
 const Footer = () => {
   return (
     <div>
-      <footer id="sticky-footer" class="flex-shrink-0 py-4 bg-dark text-white-50 footer">
-    <div class="container text-center">
+      <footer id="sticky-footer" className="flex-shrink-0 py-4 bg-dark text-white-50 footer">
+    <div className="container text-center">
       <small>Copyright &copy; All right reserved</small>
     </div>
   </footer>
---
+++ src/Components/News-card/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Card = () => {
+  return (
+    <div className="card-container">
+      <div className="card-content">
+        <div className="card-title">
+          <h2>Title of the page</h2>
+        </div>
+        <div className="card-content-inner">
+          <p>Description of the page</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
